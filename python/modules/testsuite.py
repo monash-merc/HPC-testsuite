750b1aa97db2c5a5df583a18f1e1cb952d66cbeb
internetscooter <paul.mcintosh@internetscooter.com>
2015-09-15 16:19:21 +1000

    made dir text generic

--- python/modules/testsuite.py
+++ python/modules/testsuite.py
@@ -18,26 +18,35 @@ def test_dir_exists(dir):
  
 if __name__ == '__main__':
     # dynamically create a test per module
+    # get a list of modules and exclude the header info
     modulelistp=subprocess.Popen("module -t avail 2>&1 | tail -n +13 | sed 's/(default)//'", shell=True, stdout=subprocess.PIPE)
     modulelist=modulelistp.communicate()[0].splitlines()
+    # now we create a test for each module
     for module in modulelist:
+        # first we get a list of what the module sets
         moduleshowp=subprocess.Popen("module show " + module + " 2>&1", shell=True, stdout=subprocess.PIPE)
         moduleshowlist=moduleshowp.communicate()[0].splitlines()
+        # then we test that each line makes sense
         for moduleshowline in moduleshowlist:
+            # this checks that if we define a path the path actually exists
             if moduleshowline.startswith("prepend-path"):
                 moduletokenlist = moduleshowline.split(" ")
+                #yes this needs to be made more sensible!
                 for moduletoken in moduletokenlist:
-                    if moduletoken.startswith("PATH"):
-                        dir = moduletokenlist[2] 
-                        test = test_dir_exists(dir)
-                        setattr(TestSequense, "test_" + module + "_prepend-path_PATH", test)
-                    if moduletoken.startswith("LD_LIBRARY_PATH"):
-                        dir = moduletokenlist[2]
-                        test = test_dir_exists(dir)
-                        setattr(TestSequense, "test_" + module + "_prepend-path_LD_LIBRARY_PATH", test)
-                    if moduletoken.startswith("MANPATH"):
-                        dir = moduletokenlist[2]
-                        test = test_dir_exists(dir)
-                        setattr(TestSequense, "test_" + module + "_prepend-path_MANPATH", test)
-
+                    # prepend-path always has the second to last token at the environment variable and then the dir
+                    env_variable =  moduletokenlist[-3]
+                    dir = moduletokenlist[-2]
+                    # we need to treat compiler flags in a special way and drop the -I -L etc
+                    if "FLAGS" in env_variable:
+                         dir = moduletokenlist[-2][2:]
+                         # also exclude flags that aren't paths
+                         if not "/" in dir:
+                             continue
+                    test = test_dir_exists(dir)
+                    setattr(TestSequense, "test_" + module + "_prepend-path_" + env_variable, test)                    
+            
+                    # if moduletoken.startswith("PATH"):
+                        # dir = moduletokenlist[2] 
+                        # test = test_dir_exists(dir)
+                        # setattr(TestSequense, "test_" + module + "_prepend-path_PATH", test)
     unittest.main()
